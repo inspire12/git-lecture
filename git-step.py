592020fff8c4fbf6c0ed4110e6e13257f315c198
inspire12 <ox4443@naver.com>
2025-11-18 21:54:39 +0900

    Initial get-step
    - add gitignore

---
+++ git-step.py
@@ -0,0 +1,25 @@
+
+def init_git():
+    print('git init')
+
+def add_to_stage(file_path):
+    print(f'git add {file_path}')
+
+def add_all_commit():
+    print('git commit')
+
+def add_all_commit_message_option(message):
+    print(f'git commit -m {message}')
+
+def add_ignore():
+    add_to_stage('.gitignore')
+    add_all_commit_message_option("'feat: add .gitignore'")
+
+
+if __name__ == '__main__':
+    print('Hello Git!')
+    init_git()
+    add_ignore()
+
+
+
